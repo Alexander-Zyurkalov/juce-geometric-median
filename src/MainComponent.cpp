4ad970bac3752cfd58f2e57b4b7efadfded1ac31
Alexandr Zyurkalov <alexandr.zyurkalov@idealo.de>
2022-08-24 14:31:06 +0200

    added a button

--- src/MainComponent.cpp
+++ src/MainComponent.cpp
@@ -1,1 +1,8 @@
 #include "MainComponent.h"
+
+MainComponent::MainComponent() {
+    setSize(600, 400);
+    addNewPointButton.setButtonText("Add a new point");
+    addNewPointButton.setBounds(10, 10, 100, 40);
+    addAndMakeVisible(addNewPointButton);
+}
--- src/MainComponent.h
+++ src/MainComponent.h
@@ -1,6 +1,9 @@
 #include <juce_gui_basics/juce_gui_basics.h>
 class MainComponent: public juce::Component{
+public:
+    MainComponent();
 private:
+    juce::TextButton addNewPointButton;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
 
